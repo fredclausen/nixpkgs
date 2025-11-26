4e0014bdff5dea29a598da2cf82d9349bef2ccb6
Robert Schütz <nix@dotlambda.de>
2025-11-26 15:58:29 -0800

    python3Packages.python-xbox: init at 0.1.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15463,6 +15463,8 @@ self: super: with self; {
       ;
   };
 
+  python-xbox = callPackage ../development/python-modules/python-xbox { };
+
   python-xmp-toolkit = callPackage ../development/python-modules/python-xmp-toolkit { };
 
   python-xz = callPackage ../development/python-modules/python-xz { };
